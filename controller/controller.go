086d94c1eb45d4c3383daffcb4cf901ffb293f6b
tqt1345 <tqt1345@gmail.com>
2024-10-21 01:19:11 +1100

    wip

--- controller/controller.go
+++ controller/controller.go
@@ -21,41 +21,85 @@ const (
 	ContentType = "Content-Type"
 )
 
-func Init(r *model.Repository) {
-	repo = r
+type Server struct {
+	Repo *model.Repository
+	Port string
+}
 
+// func (s *Server) HandleIndex(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello world")
+// }
+
+func (s *Server) Start() {
 	var err error
 	wd, err = os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	nv = []NavItem{}
-	nv.Add("Home", "/")
-	nv.Add("Catalogue", "/books/catalogue")
-
 	// File server
 	fs := http.FileServer(http.Dir("./static"))
 	http.HandleFunc("GET /static/", http.StripPrefix("/static/", fs).ServeHTTP)
 
 	// Index
-	http.HandleFunc("GET /", IndexHandler)
+	http.HandleFunc("GET /", s.IndexHandler)
+	http.HandleFunc("GET /api/", s.ApiIndexHandler)
+	log.Print("Starting...")
 
 	// Book handlers
-	http.HandleFunc("GET /api/", ApiIndexHandler)
-	http.HandleFunc("GET /api/books/all", ApiAllBooksHandler)
-	http.HandleFunc("GET /api/books/{id}", ApiBookByIdHandler)
-	http.HandleFunc("GET /api/books/title", ApiBookByTitleHandler)
-	http.HandleFunc("GET /books/catalogue", AllBooks)
-	http.HandleFunc("GET /books/details/{id}", BookDetailsTemplate)
+	http.HandleFunc("GET /api/books/all", s.ApiAllBooksHandler)
 
-	// Author handlers
-	http.HandleFunc("GET /api/authors/all", ApiAllAuthorsHandler)
-	http.HandleFunc("GET /api/authors/firstName", ApiAuthorByFirstName)
-
-	// Html templates
-	http.HandleFunc("GET /template/books/catalogue", AllBooksTemplate)
-	http.HandleFunc("GET /template/headers/books", BookCatalogueHeaderTemplate)
-	http.HandleFunc("GET /template/headers/index", IndexHeaderTemplate)
-	// http.HandleFunc("GET /template/books/details/{id}", BookDetailsTemplate)
+	http.HandleFunc("GET /api/books/{id}", s.ApiBookByIdHandler)
+	http.HandleFunc("GET /api/books/title", s.ApiBookByTitleHandler)
+	http.HandleFunc("GET /books/catalogue", s.AllBooks)
+	http.HandleFunc("GET /books/details/{id}", s.BookDetailsTemplate)
+	//
+	// // Author handlers
+	// http.HandleFunc("GET /api/authors/all", ApiAllAuthorsHandler)
+	// http.HandleFunc("GET /api/authors/firstName", ApiAuthorByFirstName)
+	//
+	// // Html templates
+	// http.HandleFunc("GET /template/books/catalogue", AllBooksTemplate)
+	// http.HandleFunc("GET /template/headers/books", BookCatalogueHeaderTemplate)
+	// http.HandleFunc("GET /template/headers/index", IndexHeaderTemplate)
+	// // http.HandleFunc("GET /template/books/details/{id}", BookDetailsTemplate)
+	http.ListenAndServe(s.Port, nil)
 }
+
+// func Init(r *model.Repository) {
+// 	repo = r
+//
+// 	var err error
+// 	wd, err = os.Getwd()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	nv = []NavItem{}
+// 	nv.Add("Home", "/")
+// 	nv.Add("Catalogue", "/books/catalogue")
+//
+// 	// File server
+// 	fs := http.FileServer(http.Dir("./static"))
+// 	http.HandleFunc("GET /static/", http.StripPrefix("/static/", fs).ServeHTTP)
+//
+// 	// Index
+// 	http.HandleFunc("GET /", IndexHandler)
+//
+// 	// Book handlers
+// 	http.HandleFunc("GET /api/", ApiIndexHandler)
+// 	http.HandleFunc("GET /api/books/all", ApiAllBooksHandler)
+// 	http.HandleFunc("GET /api/books/{id}", ApiBookByIdHandler)
+// 	http.HandleFunc("GET /api/books/title", ApiBookByTitleHandler)
+// 	http.HandleFunc("GET /books/catalogue", AllBooks)
+// 	http.HandleFunc("GET /books/details/{id}", BookDetailsTemplate)
+//
+// 	// Author handlers
+// 	http.HandleFunc("GET /api/authors/all", ApiAllAuthorsHandler)
+// 	http.HandleFunc("GET /api/authors/firstName", ApiAuthorByFirstName)
+//
+// 	// Html templates
+// 	http.HandleFunc("GET /template/books/catalogue", AllBooksTemplate)
+// 	http.HandleFunc("GET /template/headers/books", BookCatalogueHeaderTemplate)
+// 	http.HandleFunc("GET /template/headers/index", IndexHeaderTemplate)
+// 	// http.HandleFunc("GET /template/books/details/{id}", BookDetailsTemplate)
+// }
--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"log"
-	"net/http"
-
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/tqt1345/Library-Go/controller"
 	"github.com/tqt1345/Library-Go/model"
@@ -17,41 +12,9 @@ const (
 	ContentType = "Content-Type"
 )
 
-var (
-	repo *model.Repository
-	ctx  context.Context
-)
-
-// This exists to init everything the program needs in one area and return any errors
-func run() error {
-	ctx = context.Background()
-
-	db, err := sql.Open("sqlite3", "./data.db")
-	if err != nil {
-		return err
-	}
-
-	if err := db.Ping(); err != nil {
-		return err
-	}
-
-	log.Print("Database connected...")
-
-	repo = model.NewRepo(db)
-	log.Print("Repository loaded...")
-
-	port := ":8080"
-
-	controller.Init(repo)
-	log.Print("Routes loaded...")
-
-	log.Printf("Server started on port%s", port)
-	http.ListenAndServe(port, nil)
-	return nil
-}
-
 func main() {
-	if err := run(); err != nil {
-		log.Fatal(err)
-	}
+	db := model.NewDB()
+	repo := &model.Repository{Db: db}
+	server := controller.Server{Repo: repo, Port: ":8080"}
+	server.Start()
 }
